65cd90439e4c19c3696dbc5a98ade41bb25ec8df
vadimzachesov <vadimzachesov@yandex.ru>
2025-10-14 17:23:54 +0300

    fun

--- config.py
+++ config.py
@@ -1,16 +1,32 @@
 NUM_BLOCKS = 100  # Количество блоков
-MASS = 0.1  # Масса одного блока (кг)
-SPRING_CONSTANT = 2.0  # Жесткость пружин (Н/м)
-BLOCK_SPACING = 1.0 # Равновесное расстояние между блоками (м)
+
+# Выберите режим генерации: 'uniform' (все одинаковые) или 'random' (случайные)
+GENERATION_MODE = 'uniform'
+
+DEFAULT_MASS = 0.1  # Масса одного блока (кг)
+DEFAULT_SPRING_CONSTANT = 2.0  # Жесткость пружин (Н/м)
+DEFAULT_BLOCK_SPACING = 1.0  # Равновесное расстояние между блоками (м)
 
 # Параметры симуляции
 SIMULATION_DURATION = 60.0  # Длительность симуляции (секунды)
 SAMPLES_PER_SECOND = 60  # Количество записей данных в секунду
 TIME_STEP = 0.001  # Шаг по времени для расчетов (должен быть маленьким для стабильности)
+DECIMAL_PLACES = 9
 
 # Начальные условия
 BLOCK_TO_DISPLACE = 0  # Индекс блока для начального смещения (первый блок)
 INITIAL_DISPLACEMENT = 0.5  # Начальное смещение от положения равновесия (м)
+
+# --- Флаг и параметры для затухания (трения) ---
+ENABLE_DAMPING = True  # Поставьте False, чтобы отключить трение
+if ENABLE_DAMPING:
+    # Это примерное время в секундах, за которое амплитуда волны упадёт в ~2.7 раза.
+    DECAY_TIME_SECONDS = 10.0
+
+    # Коэффициент затухания рассчитывается автоматически из времени
+    DAMPING_COEFFICIENT = (2 * DEFAULT_MASS) / DECAY_TIME_SECONDS
+
+VISUALIZATION_SCALE = 1.0
 
 # Выходной файл
 CSV_FILENAME = 'simulation_data.csv'
--- main.py
+++ main.py
@@ -1,5 +1,31 @@
+import numpy as np
+import pandas as pd
+
+import config
+from system_builder import build_system
+from analyze import get_all_data_about_block
 from simulation import ChainSimulation
+from visualisation_demo import preprocess_data, run_animation
 
 if __name__ == "__main__":
-    simulation = ChainSimulation()
+    masses, spring_constants, spacings = build_system()
+    simulation = ChainSimulation(
+        masses=masses,
+        spring_constants=spring_constants,
+        spacings=spacings
+    )
     simulation.run()
+
+    try:
+        df = pd.read_csv('simulation_data.csv',
+                         dtype={"time": np.float64, "block_index": np.int64, "position": np.float64,
+                                "velocity": np.float64,
+                                "acceleration": np.float64})
+    except FileNotFoundError:
+        print("Error: simulation_data.csv not found. Please run main.py first.")
+        exit()
+
+    get_all_data_about_block(df=df, spacings=spacings, block_index=99)
+
+    positions_data, velocities_data, accelerations_data = preprocess_data(filename=config.CSV_FILENAME)
+    run_animation(positions_df=positions_data, spacings=spacings, velocities_df=velocities_data, accelerations_df=accelerations_data)
